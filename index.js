23e4276fad8eeb9d45628c81abdec7ae39fab0ff
Dinis <aluno27657@ipt.pt>
2026-09-14 16:02:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.MessageCreate, (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
